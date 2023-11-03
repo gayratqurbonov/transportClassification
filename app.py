11ccf07942da4d5df8642a6330621baf4ad04086
G'ayrat Qurbonov <141934156+gayratqurbonov@users.noreply.github.com>
2023-11-03 20:57:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from fastai.vision.all import *
 import pathlib
 import plotly.express as px
-import platform
+import platform as plt
 
 plt = platform.sysem()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
