2694cf86d602702f6a786445e56a3381d0409573
G'ayrat Qurbonov <141934156+gayratqurbonov@users.noreply.github.com>
2023-11-03 21:52:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from fastai.vision.all import *
 import pathlib
 import plotly.express as px
 import platform
+from pathlib import Path
 
-import platform import pathlib 
-plt = platform.system() if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
 # title
 st.title('Transportni klassifikatsiya qiluvchi model')
