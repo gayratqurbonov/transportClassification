07f56c09f8811e7303defa1877ff468edc9ae0d0
G'ayrat Qurbonov <141934156+gayratqurbonov@users.noreply.github.com>
2023-11-03 21:06:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,22 @@ import platform
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
-
 # title
-st.title('Transportni klafikatsiya qiluvchi model')
+st.title('Transportni klassifikatsiya qiluvchi model')
+
 
 # rasmni joylash
-file = st.file_uploader('Rasm yuklash', type=['png','jpeg','gif','svg'])
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
 if file:
     st.image(file)
     # PIL convert
     img = PILImage.create(file)
-
     # model
     model = load_learner('transport_model.pkl')
 
     # prediction
     pred, pred_id, probs=model.predict(img)
-    st.success(f'Bashorat: {pred}')
+    st.success(f"Bashorat: {pred}")
     st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
 
     # plotting
