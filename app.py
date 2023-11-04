37c5a30c7463ac75825f8f0adde90bbcab20b97e
G'ayrat Qurbonov <141934156+gayratqurbonov@users.noreply.github.com>
2023-11-04 12:25:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ if file:
     
     # model
     # model = data.load_learner('transport_model.pkl')
-    model = data.learn ('transport_model.pkl')
+    model = data.learn('transport_model.pkl')
+    
 
     # prediction
     pred, pred_id, probs=model.predict(img)
