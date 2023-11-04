0039c51aea92350fd5cefddb4be32d355b108a28
G'ayrat Qurbonov <141934156+gayratqurbonov@users.noreply.github.com>
2023-11-04 12:29:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if file:
     img = PILImage.create(file)
     
     # model
-    learn = load_learner('transport_model.pkl')
+    learn = data.load_learner('transport_model.pkl')
     # model = data.load_learner('transport_model.pkl')
     # model = learn('transport_model.pkl')
     
