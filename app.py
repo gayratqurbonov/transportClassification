3f9efd0d23fde610af6f1720f75ea617e211eb0a
G'ayrat Qurbonov <141934156+gayratqurbonov@users.noreply.github.com>
2023-11-04 11:38:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if file:
     # PIL convert
     img = PILImage.create(file)
     # model
-    model = load_learner('transport_model.pkl')
+    model = st.load_learner('transport_model.pkl')
 
     # prediction
     pred, pred_id, probs=model.predict(img)
