1e1dcf99d272b24f754dfb3c0de775e7543f9ad1
G'ayrat Qurbonov <141934156+gayratqurbonov@users.noreply.github.com>
2023-11-05 08:35:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,11 @@ import pathlib
 import plotly.express as px
 import platform
 
-
-# path = Path('train')
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
-
 # title
 st.title('Transportni klassifikatsiya qiluvchi model')
-
 
 # rasmni joylash
 file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
@@ -22,10 +18,8 @@ if file:
     img = PILImage.create(file)
     
     # model
-    model = data.load_learner('transport_model.pkl')
-    # model = load_learner('transport_model.pkl')
+    model = load_learner('transport_model.pkl')
     
-
     # prediction
     pred, pred_id, probs=model.predict(img)
     st.success(f"Bashorat: {pred}")
